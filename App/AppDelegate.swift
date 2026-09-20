c3fea9680afbfc991958792243c4048ee6ec568c
R347H4CK3R <51473529+R347H4CK3R@users.noreply.github.com>
2026-09-19 21:23:50 -0500

    Restore UIKit application entry point

--- App/AppDelegate.swift
+++ App/AppDelegate.swift
@@ -1,4 +1,16 @@
 import UIKit
 
+@main
 final class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        RuntimeLog.stage("APP_START")
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = GameViewController()
+        window.makeKeyAndVisible()
+        self.window = window
+        return true
+    }
 }
